98916b20f31b004f91171d9221dd57eb34065e4c
Linh <lcahu91@gmail.com>
2021-06-30 14:13:00 -0400

    added relax

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const axios = require('axios');
 const client = new Discord.Client(); // create new client
 
 
-async function getBridgertonMeme(){
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=bridgerton&rating=g`;
+async function getMeme(searchTerm){
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=${searchTerm}&rating=g`;
     return await axios.get(url)
         .then(({data}) => {
             return data.data.embed_url;
@@ -22,14 +22,17 @@ client.on('message', async msg => {
         case "ping":
             msg.reply("pong!");
             break;
-        case "!bridgerton":
-            msg.channel.send("Lady Whistledown says: ");
-            const img = await getBridgertonMeme();
-            msg.channel.send(img);
+        case "!cat":
+            msg.channel.send("Meow: ");
+            let catImg = await getMeme('cat');
+            msg.channel.send(catImg);
             break;
+        case "!relax":
+            msg.channel.send("Remember to take a break!");
+            let relaxImg = await getMeme('relax');
+            msg.channel.send(relaxImg);
+            break; 
     }
 });
-
-
 
 client.login(process.env.CLIENT_TOKEN); // login bot using token
